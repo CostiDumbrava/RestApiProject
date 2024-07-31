ef1d35313552feeab5b72e9e16a3bd83512a1f88
DUMBRAVA <C.Dumbrava@pentanova.com>
2024-07-31 19:17:03 +0300

    commit pe master

--- src/main/java/tests/ParseJson.java
+++ src/main/java/tests/ParseJson.java
@@ -49,7 +49,7 @@ public class ParseJson {
 		
 		System.out.println(jsonPath.getString("[0]"));
 		System.out.println(jsonPath.getString("[0].employee.company"));
-		
+		//comment pentru git pe master
 	}
 	
 	
